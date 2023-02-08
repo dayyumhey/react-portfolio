e12fc6020890adb6ff01d80f90c0cf851d0e3678
dayyumhey <dayyumhey@gmail.com>
2023-02-08 19:03:28 +0530

    commit

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -2,6 +2,36 @@ import { useEffect, useState } from 'react'
 import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
 import Loader from 'react-loaders'
+import React from 'https://cdn.skypack.dev/react';
+import ReactDOM from 'https://cdn.skypack.dev/react-dom';
+
+const COLORS = ['#bbf7d0', '#99f6e4', '#bfdbfe', '#ddd6fe', '#f5d0fe', '#fed7aa', '#fee2e2'];
+const TAGS = ['HTML', 'CSS', 'JavaScript', 'Typescript', 'Tailwind', 'React', 'Next.js', 'Node.js', 'UI/UX', 'SVG', 'animation', 'webdev'];
+const DURATION = 35000;
+const ROWS = 5;
+const TAGS_PER_ROW = 7;
+
+const random = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+const shuffle = (arr) => [...arr].sort( () => .5 - Math.random() );
+
+const InfiniteLoopSlider = ({children, duration, reverse = false}) => {
+  return (
+    <div className='loop-slider' style={{
+        '--duration': `${duration}ms`,
+        '--direction': reverse ? 'reverse' : 'normal'
+      }}>
+      <div className='inner'>
+        {children}
+        {children}
+      </div>
+    </div>
+  );
+};
+
+const Tag = ({ text }) => (
+  <div className='tag'><span>#</span> {text}</div>
+);
+
 
 
 const About = () => {
@@ -37,7 +67,24 @@ const About = () => {
               I am a B.tech student learning new things
             </p>
          </div>
-        </div>
+         
+         <div className='tag-list'>
+          
+      {[...new Array(ROWS)].map((_, i) => (
+        <InfiniteLoopSlider key={i} duration={random(DURATION - 5000, DURATION + 5000)} reverse={i % 2}>
+          {shuffle(TAGS).slice(0, TAGS_PER_ROW).map(tag => (
+            <Tag text={tag} key={tag}/>
+          ))}
+        </InfiniteLoopSlider>
+      ))}
+      <div className='fade'/>
+    </div>
+        
+         </div>
+         
+         
+
+      
         let loader = <Loader type="ball-scale-ripple-multiple" />
         </>
 )
--- src/components/Work/index.js
+++ src/components/Work/index.js
@@ -1,20 +1,68 @@
+import React, {useEffect, useState} from 'react';
 import './index.scss'
-
-
 import Loader from 'react-loaders'
+import AnimatedLetters from "../AnimatedLetters";
+import workData from "../../data/work.json";
 
 const Work = () => {
+  const [letterClass, setLetterClass] = useState('text-animate');
+  
+  console.log(workData);
+
+
+  useEffect(() => {
+      setTimeout(() => {
+        setLetterClass('text-animate-hover')
+     }, 3000);
+   }, [])
+
+   const renderWork = (work) => {
+    return (
+        <div className="images-container">
+            {
+                work.map((port, idx) => {
+                    return (
+                        <div className="image-box" key={idx}>
+                            <img 
+                            src={port.cover}
+                            className="portfolio-image"
+                            alt="portfolio" />
+                            <div className="content">
+                                <p className="title">{port.name}</p>
+                                <h4 className="description">{port.description}</h4>
+                                <button
+                                    className="btn"
+                                    onClick={() => window.open(port.url)}
+                                >View</button>
+                            </div>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    );
+}
 
     
         return (
             <>
-              <div>
-                <h1>hello</h1>
+              <div className="container work-page">
+                <h1 className='page-title'>
+                <AnimatedLetters
+                        letterClass={letterClass}
+                        strArray={"Portfolio".split("")}
+                        idx={15}
+                    />
+
+                </h1>
+                <div>{renderWork(workData.work)}</div>
 
               </div>
               let loader = <Loader type="ball-scale-ripple-multiple" />
               </>
       )
+
+     
 
             
 
